89d624356df2e5aa230390e54d716e000b366a17
kriskross2893 <kristofer.lora.dev@gmail.com>
2018-11-18 20:02:26 +0800

    Client - add semantic ui css for offline development

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import {Router} from 'react-router-dom'
 import createBrowserHistory from 'history/createBrowserHistory'
 import {Provider} from 'react-redux'
+import 'semantic-ui-css/semantic.min.css'
 import {
   createStore
   // applyMiddleware
