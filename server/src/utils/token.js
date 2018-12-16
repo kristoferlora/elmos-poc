471c37dd0b5bfa92c02b33b8e5bac0a6bc133fd8
kristoferlora <kristofer.lora@gmail.com>
2018-12-16 13:54:39 +0800

    Remove TTL for token

--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -8,7 +8,7 @@ import uuid from 'uuid/v4'
 import db from '../models'
 
 const DEFAULT_TOKEN_EXPIRY = 60 * 60 // 60 minutes
-const TOKEN_EXPIRY_REFRESH_THRESHOLD = 60 * 30 // 30 minutes
+// const TOKEN_EXPIRY_REFRESH_THRESHOLD = 60 * 30 // 30 minutes
 
 const verifyToken = async (token) => {
   return await jwt.verify(token, process.env.AUTH_SECRET)
@@ -30,38 +30,40 @@ const generateToken = async (payload, options) => {
 }
 
 const generateUserToken = async (user, expiry = DEFAULT_TOKEN_EXPIRY, extra) => {
-  const type = user.type ? 'ADMIN' : 'USER'
+  const model = db.User
+
   const payload = {
     userID: user.userID,
     name: `${user.firstName} ${user.lastName}`,
-    type,
+    type: db.RecordType.User.VALUES[user.recordTypeID],
     ...extra
   }
-
+  /* Enable if implementing refresh token logic */
   const options = {
-    expiresIn: expiry
+  //   expiresIn: expiry
   }
   return generateToken(payload, options)
 }
 
 const refreshToken = async (token) => {
-  const currentTime = new Date().getTime() / 1000
-  const payload = await jwt.decode(token)
-  const aboutToExpire = (payload
-    && (currentTime + TOKEN_EXPIRY_REFRESH_THRESHOLD) >= payload.exp
-    && currentTime <= payload.exp
-  )
+  /* Enable if implementing refresh token logic */
+  // const currentTime = new Date().getTime() / 1000
+  // const payload = await jwt.decode(token)
+  // const aboutToExpire = (payload
+  //   && (currentTime + TOKEN_EXPIRY_REFRESH_THRESHOLD) >= payload.exp
+  //   && currentTime <= payload.exp
+  // )
 
-  if (aboutToExpire) {
-    const options = {
-      jwtid: uuid(),
-      expiresIn: DEFAULT_TOKEN_EXPIRY
-    }
-    return await jwt.sign(_.omit(payload, ['exp', 'jti', 'exp', 'iat']),
-      process.env.AUTH_SECRET,
-      options
-    )
-  }
+  // if (aboutToExpire) {
+  //   const options = {
+  //     jwtid: uuid(),
+  //     expiresIn: DEFAULT_TOKEN_EXPIRY
+  //   }
+  //   return await jwt.sign(_.omit(payload, ['exp', 'jti', 'exp', 'iat']),
+  //     process.env.AUTH_SECRET,
+  //     options
+  //   )
+  // }
 }
 
 export {
